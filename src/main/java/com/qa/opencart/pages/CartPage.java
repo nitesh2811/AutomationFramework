aaa13f36da868d3b3bd54ed728584409c6887ba5
nitesh281189 <nagrawal512@gmail.com>
2022-07-16 14:59:14 +0530

    feature 1

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.By;
 
 public class CartPage {
 
-	
-	private By cartPage=By.id("title");
+	private By cartPage = By.id("title");
+
 	public static void main(String[] args) {
-	System.out.println("Cart Page");
+		System.out.println("Cart Page");
+
+		String s1 = "feature one";
+		System.out.println(s1);
 
 	}
+
 	public static void cartPage() {
 		System.out.println("Cart Page");
 	}
