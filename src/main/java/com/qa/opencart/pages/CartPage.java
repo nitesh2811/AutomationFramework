0a1ba5d0d0227ff47931fa3755c4f3ec94564fe5
nitesh281189 <nagrawal512@gmail.com>
2022-07-16 14:03:20 +0530

    changes done in cart page after review

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,10 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
+	
+	private By cartPage=By.id("title");
 	public static void main(String[] args) {
 	System.out.println("Cart Page");
 
 	}
-
+	public static void cartPage() {
+		System.out.println("Cart Page");
+	}
 }
