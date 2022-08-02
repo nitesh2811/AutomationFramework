05fa5981f985414cdf7a232d8a3bd0b5277d82ce
nitesh2811 <Nagrawal281189@gmail.com>
2022-08-02 07:01:44 +0530

    02082022

--- src/main/java/com/qa/opencart/exceptions/DriverException.java
+++ src/main/java/com/qa/opencart/exceptions/DriverException.java
@@ -2,6 +2,8 @@ package com.qa.opencart.exceptions;
 
 public class DriverException extends FrameWorkException{
 
+	private static final long serialVersionUID = 8588146972626877421L;
+
 	public DriverException(String message) {
 		super(message);
 		
